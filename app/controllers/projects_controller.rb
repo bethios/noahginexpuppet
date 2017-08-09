73cc1176b2bb58caa51bafffcd7d41d4bc83fb27
Beth Cummings <bethios@gmail.com>
2017-08-09 10:51:23 -0500

    test test test

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -18,19 +18,6 @@ class ProjectsController < ApplicationController
     end
   end
 
-  def edit
-  end
-
-  def update
-    if @project.update_attributes(project_params)
-      flash[:notice] = "Project was updated"
-      redirect_to admin_path
-    else
-      flash.now[:alert] = @project.errors.full_messages.to_sentence
-      render :new
-    end
-  end
-
   def destroy
     if @project.destroy
       flash[:notice] = "Project was deleted"
--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -1,6 +1,44 @@
 require 'rails_helper'
+include SessionsHelper
 
 RSpec.describe WelcomeController, type: :controller do
+  let(:admin) { User.create!(name: 'admin user', email: 'admin@admin.com', password: "helloworld")}
+
+  context "guest" do
+    describe "GET index" do
+      it "renders the index template" do
+        get :index
+        expect(response).to render_template("index")
+      end
+    end
+
+    describe "GET admin" do
+      it "redirects to new session" do
+        get :admin
+        expect(response).to redirect_to(new_session_path)
+      end
+    end
+  end
+
+  context "admin" do
+    before do
+      create_session(admin)
+    end
+
+    describe "GET index" do
+      it "renders the index template" do
+        get :index
+        expect(response).to render_template("index")
+      end
+    end
+
+    describe "GET admin" do
+      it "renders the admin template" do
+        get :admin
+        expect(response).to render_template("admin")
+      end
+    end
+  end
 
 
 end
